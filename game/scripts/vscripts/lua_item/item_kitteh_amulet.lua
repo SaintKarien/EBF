19d60127c08717f33fdfc786de59e7b0f07e99c9
SaintKarien <38432516+SaintKarien@users.noreply.github.com>
2018-05-15 02:45:46 +0800

    Update item_kitteh_amulet.lua

--- game/scripts/vscripts/lua_item/item_kitteh_amulet.lua
+++ game/scripts/vscripts/lua_item/item_kitteh_amulet.lua
@@ -1,5 +1,4 @@
 item_kitteh_amulet = class({})
-LinkLuaModifier( "modifier_item_kitteh_amulet_passive", "lua_item/item_kitteh_amulet.lua" ,LUA_MODIFIER_MOTION_NONE )
 
 function item_kitteh_amulet:OnSpellStart()
 	local caster = self:GetCaster()
@@ -12,13 +11,16 @@ function item_kitteh_amulet:OnSpellStart()
 		print( 9999 )
 		targetPos = caster:GetAbsOrigin() + direction * 9999
 	end
+
 	EmitSoundOn("DOTA_Item.BlinkDagger.Activate", caster)
 	ParticleManager:FireParticle("particles/econ/events/ti6/blink_dagger_start_ti6_lvl2.vpcf", PATTACH_ABSORIGIN, caster, {[0] = caster:GetAbsOrigin()})
 	FindClearSpaceForUnit(caster, targetPos, true)
 	ProjectileManager:ProjectileDodge( caster )
 	ParticleManager:FireParticle("particles/econ/events/ti6/blink_dagger_end_ti6_lvl2.vpcf", PATTACH_ABSORIGIN, caster, {[0] = caster:GetAbsOrigin()})
 	EmitSoundOn("DOTA_Item.BlinkDagger.NailedIt", caster)
-  
+	
+	local illusion = caster:ConjureImage( ogPos, 30, 0, 0 )
+	illusion:SetThreat( caster:GetThreat() )
 	caster:SetThreat( 0 )
 	caster:RefreshAllCooldowns(true)
 end
@@ -28,19 +30,21 @@ function item_kitteh_amulet:GetIntrinsicModifierName()
 	return "modifier_item_kitteh_amulet_passive"
 end
 
-modifier_item_kitteh_amulet_passive = class({})
 
+modifier_item_kitteh_amulet_passive = class({})
+LinkLuaModifier( "modifier_item_kitteh_amulet_passive", "items/item_kitteh_amulet.lua" ,LUA_MODIFIER_MOTION_NONE )
 
 function modifier_item_kitteh_amulet_passive:DeclareFunctions()
 	return {MODIFIER_PROPERTY_SPELL_AMPLIFY_PERCENTAGE,
 			MODIFIER_PROPERTY_COOLDOWN_PERCENTAGE_STACKING,
+			MODIFIER_PROPERTY_COOLDOWN_PERCENTAGE,
 			MODIFIER_PROPERTY_MANA_REGEN_CONSTANT,
 			MODIFIER_EVENT_ON_TAKEDAMAGE,
-			MODIFIER_PROPERTY_MOVESPEED_BONUS_UNIQUE
+			MODIFIER_PROPERTY_MOVESPEED_BONUS_UNIQUE,
 			MODIFIER_PROPERTY_MOVESPEED_ABSOLUTE_MIN,
 			MODIFIER_PROPERTY_MOVESPEED_LIMIT,
 			MODIFIER_PROPERTY_MOVESPEED_MAX,
-			MODIFIER_PROPERTY_MOVESPEED_BONUS_UNIQUE,
+			MODIFIER_PROPERTY_MOVESPEED_BONUS_CONSTANT,
 			MODIFIER_EVENT_ON_ATTACK_LANDED,
 			MODIFIER_PROPERTY_ATTACK_RANGE_BONUS_UNIQUE,
 			MODIFIER_PROPERTY_ATTACK_RANGE_BONUS,
@@ -50,7 +54,7 @@ function modifier_item_kitteh_amulet_passive:DeclareFunctions()
 			MODIFIER_PROPERTY_HEALTH_BONUS,
 			MODIFIER_PROPERTY_BASEDAMAGEOUTGOING_PERCENTAGE,
 			MODIFIER_PROPERTY_HEALTH_REGEN_PERCENTAGE,
-			MODIFIER_PROPERTY_MANA_REGEN_PERCENTAGE
+			MODIFIER_PROPERTY_MANA_REGEN_PERCENTAGE,
 			MODIFIER_PROPERTY_HEALTH_REGEN_CONSTANT,
 			MODIFIER_PROPERTY_STATS_STRENGTH_BONUS,
 			MODIFIER_PROPERTY_STATS_AGILITY_BONUS,
@@ -83,19 +87,18 @@ function modifier_item_kitteh_amulet_passive:GetModifierManaRegenPercentage()
 end
 
 function modifier_item_kitteh_amulet_passive:OnTakeDamage(params)
-	if params.attacker == self:GetParent() and not params.inflictor then
+	if params.attacker == self:GetParent() and params.unit ~= self:GetParent() and self:GetParent():GetHealth() > params.damage then
 		local flHeal = params.damage * 100
+		if params.inflictor then ParticleManager:FireParticle ("particles/items3_fx/octarine_core_lifesteal.vpcf", PATTACH_ABSORIGIN_FOLLOW, self) end
 		params.attacker:HealEvent(flHeal, self:GetAbility(), params.attacker)
-	end
-
-	if params.attacker == self:GetParent() then
-		local flHeal = params.damage * 100
-		if params.inflictor then ParticleManager:FireParticle("particles/items3_fx/octarine_core_lifesteal.vpcf", PATTACH_ABSORIGIN_FOLLOW, self) end
-		params.attacker:HealEvent(flHeal, self:GetAbility(), params.attacker)
-	end
+		end
 end
 
 function modifier_item_kitteh_amulet_passive:GetModifierMoveSpeed_AbsoluteMin()
+	return 550
+end
+
+function modifier_item_kitteh_amulet_passive:GetModifierMoveSpeedBonus_Constant()
 	return 550
 end
 
@@ -113,64 +116,49 @@ end
 
 function modifier_item_kitteh_amulet_passive:OnAttackLanded(params)
 	if IsServer() then
-		if params.attacker == self:GetParent() and RollPercentage(95) then
-			self:GetAbility():DealDamage(self:GetParent(), params.target, 100, {damage_type = DAMAGE_TYPE_PURE})
-
-		end
-	end
-
-
-	if IsServer() then
-		if not params.attacker == self:GetParent() and self:GetAbility():IsCooldownReady() then
-			local parent = self:GetParent()
-			parent:StartGestureWithPlaybackRate(ACT_DOTA_ATTACK, 17)
-			self:GetAbility():SetCooldown()
-			Timers:CreateTimer(0.1, function() parent:PerformGenericAttack(params.target, true) end)
-		end
-	end
-
-	if IsServer() then
 		if params.attacker == self:GetParent() then
-			for _, enemy in ipairs( self:GetParent():FindEnemyUnitsInRadius( params.target:GetAbsOrigin(), 550) ) do
-				if enemy ~= params.target then
-					self:GetAbility():DealDamage( self:GetParent(), enemy, params.original_damage * 1, {damage_type = DAMAGE_TYPE_PHYSICAL})
-				end
-			end
-		end
-	end
-
-
-	if IsServer() then
-		if params.attacker == self:GetParent() and RollPercentage(50) then
-			params.target:DisableHealing(8)
+			local parent = self:GetParent()
+			parent:StartGestureWithPlaybackRate(ACT_DOTA_ATTACK, 1)
+				Timers:CreateTimer(0.1, function()
+				EmitSoundOn("DOTA_Item.SkullBasher", params.target)
+				parent:PerformGenericAttack(params.target, true, 0, false, true)  
+				self:GetAbility():Stun(params.target, 1, true)
+				GetAbility():DealDamage(self:GetParent(), params.target, 100, {damage_type = DAMAGE_TYPE_PURE})
+				params.target:DisableHealing(8)
+			end)
 		end
 	end
 
 end
 
-
 function modifier_item_kitteh_amulet_passive:GetModifierAttackRangeBonusUnique()
-		return 8000
+		return 800
 end
 
 
 function modifier_item_kitteh_amulet_passive:GetModifierPercentageCooldownStacking()
-	return 95
+		return 95
 end
 
+
+function modifier_item_kitteh_amulet_passive:GetModifierPercentageCooldown()
+		return 95
+end
+
+
 function modifier_item_kitteh_amulet_passive:GetModifierTotal_ConstantBlock()
-	if RollPercentage(100) then
 		return 500
 end
 
 
 function modifier_item_kitteh_amulet_passive:GetModifierAttackRangeBonus()
-	return 8000
+		return 500
 end
 
 function modifier_item_kitteh_amulet_passive:GetModifierPreAttack_CriticalStrike()
 	if RollPercentage(95) then
 		return 500
+	end
 end
 
 function modifier_item_kitteh_amulet_passive:GetModifierHealthBonus()
@@ -182,29 +170,27 @@ function modifier_item_kitteh_amulet_passive:GetModifierBaseDamageOutgoing_Perce
 end
 
 function modifier_item_kitteh_amulet_passive:GetModifierBonusStats_Intellect()
-	return 88888
+	return 888
 end
 
 function modifier_item_kitteh_amulet_passive:GetModifierBonusStats_Strength()
-	return 88888
+	return 888
 end
 
 function modifier_item_kitteh_amulet_passive:GetModifierBonusStats_Agility()
-	return 88888
+	return 888
 end
 
 function modifier_item_kitteh_amulet_passive:GetModifierCastRangeBonus()
-	return 8000
+	return 5000
 end
-
 
 function modifier_item_kitteh_amulet_passive:GetModifierCastRangeBonusTarget()
-	return 8000
+	return 5000
 end
 
-
 function modifier_item_kitteh_amulet_passive:GetModifierCastRangeBonusStacking()
-	return 8000
+	return 5000
 end
 
 function modifier_item_kitteh_amulet_passive:GetModifierMoveSpeedBonus_Percentage()
@@ -214,7 +200,6 @@ end
 function modifier_item_kitteh_amulet_passive:IsHidden()
 	return true
 end
-
 
 function modifier_item_kitteh_amulet_passive:GetAttributes()
 	return MODIFIER_ATTRIBUTE_MULTIPLE
